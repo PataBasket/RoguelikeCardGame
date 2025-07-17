de01f2eef814ca2c5ee021c62ba87e31d96edcec
PataBasket <basket2323yuta@gmail.com>
2025-07-17 17:49:31 +0900

    Add: :sparkles: カード生成時のInputHandlerの追加

---
+++ Assets/Scripts/Views/InfoInputHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoInputHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject imageSelectionPanel;
+    [SerializeField] private GameObject cardGeneratingPanel;
+    [SerializeField] private GameObject cardResultPanel;
+
+    [SerializeField] private Text cardTitle;
+    [SerializeField] private Text cardExplanation;
+    [SerializeField] private Image imageChosen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // 「写真を撮る」ボタンが押されたら実行される
+    public void OnClickTakePicture()
+    {
+        
+    }
+    
+    //　「画像を選択」ボタンが押されたら実行される
+    public void OnClickChoosePicture()
+    {
+        
+    }
+
+    // 「この内容で作成する」ボタンが押されたら実行される
+    public void OnClickSubmitCard()
+    {
+        
+    } 
+}
