f69063f7ad37182e486444ded65a204e1cb3c6b6
tsuzuki-seita <continew2002@gmail.com>
2025-07-20 14:23:08 +0900

    feat: バトル完成

--- Assets/Scripts/Views/PlayerBattleView.cs
+++ Assets/Scripts/Views/PlayerBattleView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -67,22 +68,56 @@ public class PlayerBattleView : MonoBehaviour
     {
         hpText.text = $"{hp}";
         hpBar.fillAmount = hp / 100f; // HPの最大値を100と仮定
+
+        // カード画像を小刻みに振動（ダメージ表現）
+        cardImage.rectTransform.DOShakePosition(
+            duration: 0.3f,      // 振動の長さ
+            strength: new Vector3(30f, 0, 0),  // 横方向の振動
+            vibrato: 10,         // 振動回数
+            randomness: 90       // ランダム性
+        ).SetEase(Ease.OutQuad);
     }
 
-     public void UpdateAttackText(BattleModel.HandType handType)
+    public void UpdateAttackText(BattleModel.HandType handType)
     {
+        Button targetButton = null;
         switch (handType)
         {
             case BattleModel.HandType.Rock:
                 attackText.text = $"{rockName}";
+                targetButton = rockButton;
                 break;
             case BattleModel.HandType.Scissors:
                 attackText.text = $"{scissorsName}";
+                targetButton = scissorsButton;
                 break;
             case BattleModel.HandType.Paper:
                 attackText.text = $"{paperName}";
+                targetButton = paperButton;
                 break;
         }
+
+        if (targetButton != null)
+        {
+            RectTransform btnTransform = targetButton.GetComponent<RectTransform>();
+            // 一度拡大して戻る
+            btnTransform.DOComplete(); // 前回アニメが残ってたら止める
+            btnTransform.DOKill();     // Killも保険で
+            btnTransform.localScale = Vector3.one;
+            btnTransform.DOScale(1.5f, 0.15f).SetEase(Ease.OutBack)
+                .OnComplete(() => btnTransform.DOScale(1f, 0.15f).SetEase(Ease.InBack));
+        }
+    }
+
+    public void SuccessAttack()
+    {
+        // 一瞬だけ大きくなって戻る（攻撃表現）
+        cardImage.rectTransform.DOPunchScale(
+            punch: new Vector3(1f, 1f, 0), // 拡大率
+            duration: 0.25f,                   // 持続時間
+            vibrato: 1,                        // 一度だけ
+            elasticity: 0.5f                   // 弾力性
+        );
     }
 
     public void UpdateResult(bool result, int score)
